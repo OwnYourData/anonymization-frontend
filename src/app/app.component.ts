a1453839533d95fe410859fb7d01272b65e8d647
Paul Feichtenschlager <paul.feichtenschlager@d-fine.com>
2026-02-20 08:47:36 +0100

    Start of landing page implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { JsonLdFormComponent } from './components/json-ld-form/json-ld-form.component';
-import { FlatJsonFormComponent } from './components/flat-json-form/flat-json-form.component';
-
-import { FurtherInfoComponent } from './components/further-info/further-info.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, JsonLdFormComponent, FlatJsonFormComponent, FurtherInfoComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  activeTab: 'json-ld' | 'flat-json' = 'json-ld';
-
-  setActiveTab(tab: 'json-ld' | 'flat-json'): void {
-    this.activeTab = tab;
-  }
-}
+export class AppComponent { }
